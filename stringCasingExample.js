5e41c334e2ad38c9b02ece589b3981c60c96222b
Tim Kirves <tim.kirves@gmail.com>
2021-02-16 23:00:01 +0200

    example casing name added

---
+++ stringCasingExample.js
@@ -0,0 +1,8 @@
+// Example of String Casing Name
+
+var greeting = prompt("Please enter your name");
+
+var firstChar = greeting.slice(0,1).toUpperCase();
+var lastChar  = greeting.slice(1,greeting.length).toLowerCase();
+
+alert("Hello "+ firstChar + lastChar);
