b086ecb6f09a24ca20ab19550127b6d1845ddeb2
Tim Kirves <tim.kirves@gmail.com>
2021-02-17 10:26:42 +0200

    added 2 examples

---
+++ dogAgeCalculator.js
@@ -0,0 +1,10 @@
+//DogVSHuman age converter
+
+// 1. User needs to enter dogAge
+// 2. Calcualtion
+// 3. user needs to see result/ outcome
+
+var userInput =  prompt("Please enter dog age");
+var dogAge = userInput;
+var humanAge = ((dogAge-2)*4)+21;
+alert(humanAge);
--- stringCasingExample.js
+++ stringCasingExample.js
@@ -5,4 +5,6 @@ var greeting = prompt("Please enter your name");
 var firstChar = greeting.slice(0,1).toUpperCase();
 var lastChar  = greeting.slice(1,greeting.length).toLowerCase();
 
-alert("Hello "+ firstChar + lastChar);
+var fullName = firstChar+lastChar;
+
+alert("Hello " + fullName);
