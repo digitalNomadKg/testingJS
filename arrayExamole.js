783e1b054e44ea89f9c14147c527f37386895c58
Tim Kirves <tim.kirves@gmail.com>
2021-02-21 20:27:19 +0200

    array example

--- arrayExamole.js
+++ arrayExamole.js
@@ -2,12 +2,18 @@
 //System checking the name/ array of names.
 // if user in the list, welcome, if not, sorry next time
 
-var getName = prompt("Please enter the name to check");
+function nameCheck() {
+  var getName = prompt("Please enter the name to check");
+  var nameList = ["Timur", "Manas", "Asyl", "Miku", "Emir"];
 
-const nameList = ["Timur", "Manas","Asyl", "Miku", "Emir"];
-
-if(getName===nameList){
-  alert("welcome " + getName);
-}else{
-  alert("Sorry next time");
+  if (nameList.includes(getName)) {
+    alert("Welcome " + getName);
+  } else {
+    alert("Sorry " + getName + " next time");
+  }
 }
+
+nameCheck();
+
+
+
